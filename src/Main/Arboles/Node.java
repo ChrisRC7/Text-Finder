82dfa434d957a65a71181c0c43323e7f65ecddc4
Christopher Rodríguez <chrisrc7@live.com>
2022-10-08 14:28:40 -0600

    °1 Commit, Inicio de repositorio e inicio de la logisticas de los arboles

---
+++ src/Main/Arboles/Node.java
@@ -0,0 +1,18 @@
+package Main.Arboles;
+
+public class Node<T extends Comparable<T>> {
+    T element;
+    Node<T> left;
+    Node<T> right;
+    
+    public Node(T element) {
+
+        this(element, null, null);
+    }
+
+    public Node(T element, Node<T> left, Node<T> right) {
+        this.element = element;
+        this.left = left;
+        this.right = right;
+    }
+}
