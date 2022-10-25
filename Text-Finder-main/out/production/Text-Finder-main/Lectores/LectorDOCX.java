113334b206435aed6bb71c5b6e83d5a2e41f8aab
Gaiseric <103780732+Gaiseric@users.noreply.github.com>
2022-10-25 00:27:54 -0600

    4to Commit Archivos .docx
    Se integra clase y bibliotecas para leer .docx

---
+++ Text-Finder-main/out/production/Text-Finder-main/Lectores/LectorDOCX.java
@@ -0,0 +1,29 @@
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public class LectorDOCX {
+    public static void main(String[] args){
+        try {
+            FileInputStream fis = new FileInputStream("C:\\Users\\ignac\\IdeaProjects\\testo\\src\\Prueba docx.docx");
+
+            XWPFDocument docx = new XWPFDocument(fis);
+            List<XWPFParagraph> paragraphList = docx.getParagraphs();
+
+            for(XWPFParagraph paragraph: paragraphList){
+                System.out.println(paragraph.getText());
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
